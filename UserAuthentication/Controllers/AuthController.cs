02aaf1e14e200ea0662133d9bdd2bf064e80280a
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-03-13 21:42:16 +0300

    Add project files.

---
+++ UserAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using UserAuthentication.Models;
+
+namespace UserAuthentication.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Kontroller ilk çağırıldığında, Get metodu çalışacaktır.
+            // Get Metodu , sayfa açılırken içerisinde hazır bulunması gereken veriler burada hazırlanır.
+            return View(new AuthModel()
+            {
+                 IsGet=true
+            });
+        }
+        [HttpPost]
+        public IActionResult Login(string username,string password)
+        {
+            if(username=="emrah" && password == "1010")
+            {
+                return View("Index",new AuthModel()
+                {
+                     IsSucces=true,
+                      IsGet=false
+                });
+
+            }
+            else
+            {
+
+                return View("Index",new AuthModel()
+                {,
+                     IsGet=false,
+                      IsSucces=false
+                });
+            }
+
+        }
+    }
+}
---
+++ UserAuthentication/Models/AuthModel.cs
@@ -0,0 +1,8 @@
+﻿namespace UserAuthentication.Models
+{
+    public class AuthModel
+    {
+        public bool IsGet { get; set; }
+        public bool IsSucces { get; set; }
+    }
+}
